b4f1b26596c9218ede79f4c4291dc6e656d6178e
chenwenxing <1926176613@qq.com>
2023-02-16 11:31:25 +0800

    dev提交

--- src/main/java/com/chenwenxing/Hello.java
+++ src/main/java/com/chenwenxing/Hello.java
@@ -4,5 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world1");
         System.out.println("hello world2");
+        System.out.println("hello world2");
+        System.out.println("dev");
     }
 }
